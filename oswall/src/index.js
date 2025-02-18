f54de33bc92c330357033df223f838e23c7533e5
mohammadosman <mohammadhakimosman@outlook.com>
2025-02-17 22:56:55 -0800

    fixed a mistake

--- oswall/src/index.js
+++ oswall/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './components/common/App';
 
 
 // If you want to start measuring performance in your app, pass a function
