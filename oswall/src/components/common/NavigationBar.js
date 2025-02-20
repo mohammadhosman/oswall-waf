bfe34369a2b70df89d6732a377a9483cd42ee191
mohammadosman <mohammadhakimosman@outlook.com>
2025-02-20 15:00:18 -0800

    working on navbar

--- oswall/src/components/common/NavigationBar.js
+++ oswall/src/components/common/NavigationBar.js
@@ -6,8 +6,15 @@ import NavBar from "react-bootstrap/NavBar";
 
 function NavigationBar() {
     return (
-        <NavBar bg="dark">
-
+        <NavBar bg="dark" data-bs-theme="dark" id="nav-bar">
+            <NavBar.Brand href="#home" id="nav-bar-brand">
+                <img alt="oswall logo" 
+                src={require("../../images/common/oswall-logo-no-background.png")} 
+                width="50" 
+                height="50" 
+                className="d-inline-block align-top"
+                />
+            </NavBar.Brand>
         </NavBar>
     )
 }
