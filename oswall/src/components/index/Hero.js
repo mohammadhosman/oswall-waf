a53fb209dce2866bcc4ef6d93779f6c44ea22ac7
mohammadosman <mohammadhakimosman@outlook.com>
2025-03-08 22:59:33 -0800

    m

--- oswall/src/components/index/Hero.js
+++ oswall/src/components/index/Hero.js
@@ -4,7 +4,14 @@ import "../../styling/index/Hero.css";
 function Hero() {
     return (
         <header id="hero">
+            <div id="hero-image">
+                <div className="hero-text">
+                    <h1>OsWall Web Application Firewall</h1>
+                    <p>Made to protect your web app</p>
+                    <button>Register</button>
+                </div>
 
+            </div>
         </header>
     )
 }
