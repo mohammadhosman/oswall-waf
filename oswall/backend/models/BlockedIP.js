f13957bcf250e559ce9b828085c6831922f58535
mohammadosman <mohammadhakimosman@outlook.com>
2025-06-08 17:13:44 -0700

    going to work on rate limiting feature

--- oswall/backend/models/BlockedIP.js
+++ oswall/backend/models/BlockedIP.js
@@ -4,8 +4,10 @@ const ProtectedSite = require('./ProtectedSite'); // Importing ProtectedSite mod
 // BlockedIP model schema. This will be used to store info about blocked IPs
 const BlockedIPSchema = new mongoose.Schema({
     protectedSite: {type: mongoose.Schema.Types.ObjectId, ref: 'ProtectedSite', required: true},
-    ip: {type: String, required: true, unique: true},
+    ip: {type: String, required: true},
     createdAt: {type: Date, default: Date.now}
 })
+
+BlockedIPSchema.index({ protectedSite: 1, ip: 1}, { unique: true });
 
 module.exports = mongoose.model('BlockedIP', BlockedIPSchema);
