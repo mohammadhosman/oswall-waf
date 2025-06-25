56e62148fcd80b4d692a2b55bb7a526f1407690d
mohammadosman <mohammadhakimosman@outlook.com>
2025-06-24 21:54:47 -0700

    finished security rules page

--- oswall/src/components/securityRules/SecurityRulesTable.js
+++ oswall/src/components/securityRules/SecurityRulesTable.js
@@ -17,7 +17,20 @@ function SecurityRulesTable({rules = [], onEdit, onDelete}) {
                         <td colSpan={3} className='text-center'><strong>No rules set!</strong></td>
                     </tr>
                 ): (
-                    <h1>Working on this part of table</h1>
+                    rules.map(rule => (
+                        <tr key={rule.id}>
+                            <td>{rule.limit} requests</td>
+                            <td>per {rule.window} {rule.windowUnit}{rule.window >1 ? 's' : ''}</td>
+                            <td>
+                                <Button variant='outline-primary' size='sm' className='me-2' onClick={() => onEdit(rule)}>
+                                    Edit
+                                </Button>
+                                <Button variant='outline-danger' size='sm' onClick={() => onDelete(rule.id)}>
+                                    Delete
+                                </Button>
+                            </td>
+                        </tr>
+                    ))
                 )}
             </tbody>
         </Table>
